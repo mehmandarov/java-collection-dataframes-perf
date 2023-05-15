f791adc7356b77fcb0489b30e5d9afc0bdf1f1fe
Rustam Mehmandarov <mehmandarov@gmail.com>
2023-05-15 21:49:18 +0200

    Improve synthetic data generation function

--- code/utils/src/main/java/SyntheticDataGenerator.java
+++ code/utils/src/main/java/SyntheticDataGenerator.java
@@ -2,47 +2,79 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-public class SyntheticDataGenerator {
+public class SyntheticDataGenerator
+{
 
-    public static void main(String[] args) {
-        List<String[]> syntheticData = generateSyntheticData(100);
+    final static int NUMBER_OF_SAMPLES = 100;
+    final static int CONF_NAME_LENGTH_MAX = 20;
+    final static int CONF_NAME_LENGTH_MIN = 6;
+    final static String DATE_PERIOD_START = "2022-01-01";
+    final static String DATE_PERIOD_END = "2022-12-31";
+
+
+    public static void main(String[] args)
+    {
+        List<String[]> syntheticData = generateSyntheticData(NUMBER_OF_SAMPLES);
         printDataToCSV(syntheticData);
     }
 
-    private static List<String[]> generateSyntheticData(int numRows) {
+    private static List<String[]> generateSyntheticData(int numRows)
+    {
         List<String[]> syntheticData = new ArrayList<>();
         Random random = new Random();
 
         String[] countries = {"Greece", "Poland", "United States", "Germany", "Romania", "Sweden", "WWW"};
         String[] cities = {"Athens", "Krakow", "Atlanta", "Brühl", "Bucharest", "Stockholm"};
-        String[] sessionTypes = {"talks", "workshops"};
+        String[] sessionTypes = {"talks", "workshops", "lightning talks"};
 
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 0; i < numRows; i++)
+        {
             int rndNumber = random.nextInt(countries.length);
-            String eventName = "Synthetically Generated Event " + (i + 1);
+            String eventName = randomConfName(CONF_NAME_LENGTH_MIN, CONF_NAME_LENGTH_MAX);
             String country = countries[rndNumber];
             String city = country.equals("WWW") ? "Online event" : cities[rndNumber];
 
-            String startDate = randomDate("2022-01-01", "2022-12-31");
-            String endDate = randomDate(startDate, "2022-12-31");
+            String startDate = randomDate(DATE_PERIOD_START, DATE_PERIOD_END);
+            String endDate = randomDate(startDate, DATE_PERIOD_END);
 
-            String[] sessionTypeArray = randomSessionTypes(sessionTypes);
-            String sessionTypesStr = String.join(",", sessionTypeArray);
+            String sessionTypeListString = randomSessionTypes(sessionTypes);
 
-            String[] row = {eventName, country, city, startDate, endDate, sessionTypesStr};
+            String[] row = {eventName, country, city, startDate, endDate, sessionTypeListString};
             syntheticData.add(row);
         }
 
         return syntheticData;
     }
 
-    private static String randomDate(String startDateStr, String endDateStr) {
-        try {
+    public static String randomConfName(int minLength, int maxLength)
+    {
+        Random random = new Random();
+
+        final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+        int length = random.nextInt(maxLength - minLength + 1) + minLength;
+
+        StringBuilder sb = new StringBuilder(length);
+
+        // Generate first letter as uppercase
+        char firstLetter = (char) (random.nextInt(26) + 'A');
+        sb.append(firstLetter);
+
+        // Generate remaining characters
+        for (int i = 1; i < length; i++)
+        {
+            char randomChar = CHARACTERS.charAt(random.nextInt(CHARACTERS.length()));
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    private static String randomDate(String startDateStr, String endDateStr)
+    {
+        try
+        {
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
             Date startDate = format.parse(startDateStr);
             Date endDate = format.parse(endDateStr);
@@ -54,40 +86,53 @@ public class SyntheticDataGenerator {
             Date randomDate = new Date(randomMillis);
 
             return format.format(randomDate);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
         return null;
     }
 
-    private static String[] randomSessionTypes(String[] sessionTypes) {
+    private static String randomSessionTypes(String[] sessionTypes)
+    {
+        ArrayList<String> selectedTypes = new ArrayList<>(sessionTypes.length);
         Random random = new Random();
-        int numTypes = random.nextInt(2) + 1;  // Randomly choose 1 or 2 types
+        int randomNumberOfElements = random.nextInt(sessionTypes.length + 1);
+        int counter = 0;
 
-        String[] selectedTypes = new String[numTypes];
-        List<String> sessionTypesList = List.of(sessionTypes);
-
-        for (int i = 0; i < numTypes; i++) {
-            String type = sessionTypesList.get(random.nextInt(sessionTypesList.size()));
-            selectedTypes[i] = type;
+        for (int i = sessionTypes.length - 1; i >= 0; i--)
+        {
+            int randomIndex = random.nextInt(i + 1);
+            String sessionType = sessionTypes[randomIndex];
+            if (counter <= randomNumberOfElements && !selectedTypes.contains(sessionType))
+            {
+                selectedTypes.add(sessionType);
+                counter++;
+            }
         }
 
-        return selectedTypes;
+        return "\"" + selectedTypes + "\"";
     }
 
-    private static void printDataToCSV(List<String[]> data) {
+    private static void printDataToCSV(List<String[]> data)
+    {
         String csvFileName = "synthetic_data.csv";
-        try (PrintWriter writer = new PrintWriter(new FileWriter(csvFileName))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(csvFileName)))
+        {
             String header = "Event Name,Country,City,Start Date,End Date,Session Types";
             writer.println(header);
 
-            for (String[] row : data) {
+            for (String[] row : data)
+            {
                 String rowStr = String.join(",", row);
                 writer.println(rowStr);
             }
 
             System.out.println("CSV file '" + csvFileName + "' has been generated.");
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
