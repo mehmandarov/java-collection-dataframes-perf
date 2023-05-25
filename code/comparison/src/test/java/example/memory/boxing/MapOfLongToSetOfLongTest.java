00b33cbe6befca77ca4636f9304b4c4fd79c498e
Donald Raab <Donald.Raab@bnymellon.com>
2023-05-25 15:18:22 -0400

    Fixed formatting

--- code/comparison/src/test/java/example/memory/boxing/MapOfLongToSetOfLongTest.java
+++ code/comparison/src/test/java/example/memory/boxing/MapOfLongToSetOfLongTest.java
@@ -14,17 +14,16 @@ public class MapOfLongToSetOfLongTest
     @Test
     public void maps()
     {
-PrimitiveIterator.OfLong random1 = new Random(0L).longs(10_000L, 2_000_000L).iterator();
-Map<Long, Set<Long>> map = new HashMap<>();
-LongInterval lim = LongInterval.oneTo(200_000L);
-LongInterval lis = LongInterval.oneTo(10);
-lim.forEach(each -> map.put(each, lis.collect(l -> random1.next(), new HashSet<>())));
-System.out.println(GraphLayout.parseInstance(map).toFootprint());
+        PrimitiveIterator.OfLong random1 = new Random(0L).longs(10_000L, 2_000_000L).iterator();
+        Map<Long, Set<Long>> map = new HashMap<>();
+        LongInterval lim = LongInterval.oneTo(200_000L);
+        LongInterval lis = LongInterval.oneTo(10);
+        lim.forEach(each -> map.put(each, lis.collect(l -> random1.next(), new HashSet<>())));
+        System.out.println(GraphLayout.parseInstance(map).toFootprint());
 
-PrimitiveIterator.OfLong random2 = new Random(0L).longs(10_000L, 2_000_000L).iterator();
-MutableLongObjectMap<MutableLongSet> pMap = LongObjectMaps.mutable.empty();
-lim.forEach(each -> pMap.put(each, lis.asLazy().collectLong(l -> random2.next()).toSet()));
-System.out.println(GraphLayout.parseInstance(pMap).toFootprint());
+        PrimitiveIterator.OfLong random2 = new Random(0L).longs(10_000L, 2_000_000L).iterator();
+        MutableLongObjectMap<MutableLongSet> pMap = LongObjectMaps.mutable.empty();
+        lim.forEach(each -> pMap.put(each, lis.asLazy().collectLong(l -> random2.next()).toSet()));
+        System.out.println(GraphLayout.parseInstance(pMap).toFootprint());
     }
-
 }
