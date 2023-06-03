6f0b8ac1aef93db3d75039a1554e31cdd270ecd2
Donald Raab <Donald.Raab@bnymellon.com>
2023-06-03 02:01:23 -0400

    Add some more small collection memory tests

--- code/comparison/src/test/java/example/memory/collections/SmallListsMemoryTest.java
+++ code/comparison/src/test/java/example/memory/collections/SmallListsMemoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jol.info.GraphLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SmallListsMemoryTest
@@ -29,6 +30,75 @@ public class SmallListsMemoryTest
         };
         Assertions.assertEquals(776L, GraphLayout.parseInstance(array).totalSize());
         System.out.println(GraphLayout.parseInstance(array).toFootprint());
+    }
+
+    @Test
+    public void fixedSizeListsToSizeSix()
+    {
+        ArrayList arrayList = new ArrayList();
+        System.out.println("ArrayList Empty: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList 1: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList 2: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList 3: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList 4: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList 5: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList 6: " + GraphLayout.parseInstance(arrayList).totalSize());
+
+        arrayList = new ArrayList(0);
+        System.out.println("ArrayList 0: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList.add(null);
+        System.out.println("ArrayList0 1: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList = new ArrayList(0);
+        arrayList.add(null);
+        arrayList.add(null);
+        System.out.println("ArrayList0 2: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList = new ArrayList(0);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        System.out.println("ArrayList0 3: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList = new ArrayList(0);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        System.out.println("ArrayList0 4: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList = new ArrayList(0);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        System.out.println("ArrayList0 5: " + GraphLayout.parseInstance(arrayList).totalSize());
+        arrayList = new ArrayList(0);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add(null);
+        System.out.println("ArrayList0 6: " + GraphLayout.parseInstance(arrayList).totalSize());
+
+        List list = Lists.fixedSize.empty();
+        System.out.println("FixedSizeList Empty: " + GraphLayout.parseInstance(list).totalSize());
+        list = Lists.fixedSize.of((Object)null);
+        System.out.println("FixedSizeList 1: " + GraphLayout.parseInstance(list).totalSize());
+        list = Lists.fixedSize.of(null, null);;
+        System.out.println("FixedSizeList 2: " + GraphLayout.parseInstance(list).totalSize());
+        list = Lists.fixedSize.of(null, null, null);
+        System.out.println("FixedSizeList 3: " + GraphLayout.parseInstance(list).totalSize());
+        list = Lists.fixedSize.of(null, null, null, null);
+        System.out.println("FixedSizeList 4: " + GraphLayout.parseInstance(list).totalSize());
+        list = Lists.fixedSize.of(null, null, null, null, null);
+        System.out.println("FixedSizeList 5: " + GraphLayout.parseInstance(list).totalSize());
+        list = Lists.fixedSize.of(null, null, null, null, null, null);
+        System.out.println("FixedSizeList 6: " + GraphLayout.parseInstance(list).totalSize());
     }
 
     @Test
--- code/comparison/src/test/java/example/memory/collections/SmallSetsMemoryTest.java
+++ code/comparison/src/test/java/example/memory/collections/SmallSetsMemoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jol.info.GraphLayout;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class SmallSetsMemoryTest
@@ -38,5 +39,64 @@ public class SmallSetsMemoryTest
         };
         Assertions.assertEquals(448L, GraphLayout.parseInstance(array).totalSize());
         System.out.println(GraphLayout.parseInstance(array).toFootprint());
+    }
+
+    @Test
+    public void fixedSizeSetsToSizeFour()
+    {
+        HashSet hashSet = new HashSet();
+        System.out.println("HashSet Empty: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet.add(new Object());
+        System.out.println("HashSet 1: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet.add(new Object());
+        System.out.println("HashSet 2: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet.add(new Object());
+        System.out.println("HashSet 3: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet.add(new Object());
+        System.out.println("HashSet 4: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+
+        hashSet = new HashSet(0);
+        System.out.println("HashSet 0: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet.add(new Object());
+        System.out.println("HashSet0 1: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet = new HashSet(0);
+        hashSet.add(new Object());
+        hashSet.add(new Object());
+        System.out.println("HashSet0 2: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet = new HashSet(0);
+        hashSet.add(new Object());
+        hashSet.add(new Object());
+        hashSet.add(new Object());
+        System.out.println("HashSet0 3: " +
+                GraphLayout.parseInstance(hashSet).totalSize());
+        hashSet = new HashSet(0);
+        hashSet.add(new Object());
+        hashSet.add(new Object());
+        hashSet.add(new Object());
+        hashSet.add(new Object());
+        System.out.println("HashSet0 4: " + GraphLayout.parseInstance(hashSet).totalSize());
+
+        Set set = Sets.fixedSize.empty();
+        System.out.println("FixedSizeSet Empty: " + GraphLayout.parseInstance(set).totalSize());
+        set = Sets.fixedSize.of(new Object());
+        System.out.println("FixedSizeSet 1: " +
+                GraphLayout.parseInstance(set).totalSize());
+        set = Sets.fixedSize.of(new Object(), new Object());;
+        System.out.println("FixedSizeSet 2: " +
+                GraphLayout.parseInstance(set).totalSize());
+        set = Sets.fixedSize.of(new Object(), new Object(), new Object());
+        System.out.println("FixedSizeSet 3: " +
+                GraphLayout.parseInstance(set).totalSize());
+        set = Sets.fixedSize.of(new Object(), new Object(), new Object(), new Object());
+        System.out.println("FixedSizeSet 4: " +
+                GraphLayout.parseInstance(set).totalSize());
     }
 }
